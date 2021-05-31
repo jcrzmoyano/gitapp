bd5da6a37ab2a9dd2d233ae09e0c1e61529c8da9
Diego <diemolina@gmail.com>
2021-05-30 21:59:55 -0300

    Test para Movimiento Eliminar

--- tests/integration/index.test.js
+++ tests/integration/index.test.js
@@ -291,3 +291,29 @@ test('Editar movimiento inexistente por api', async () => {
 
     expect(req.status).toBe(404);
 });
+// -> Begin test
+test('Eliminar movimiento por api', async () => {
+    const movementData = {
+        date: '04/01/2021',
+        amount: 50000.0,
+        type: MovementType.INCOME,
+        category: 'Sueldo',
+    };
+
+    // Creamos el movimiento
+    const movement = await MovementModel.create(movementData);
+
+    const URL = `${baseURL}/movements/${movement.id}`;
+    const req = await fetch(URL, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movementData),
+    });
+    const response = await req.json();
+    expect(response.message).toMatch("Movimiento eliminado");
+    expect(req.status).toBe(200);
+
+});
+//<- End Test
